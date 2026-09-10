6eddf840f4924103c08245695b17626f82229949
Mohammed Ameer <ameer6293@gmail.com>
2026-09-10 17:05:45 +0530

    chore(cleanup): deleted temporary patch scripts and replacements to clean up workspace

--- test_pnl.py
+++
@@ -1,8 +0,0 @@
-import requests
-import time
-from core.pnl_generator import generate_pnl_image
-
-def test_telegram_send():
-    print('Ready to patch telegram bot service')
-
-test_telegram_send()
